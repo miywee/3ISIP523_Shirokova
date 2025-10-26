f2c83acdbadb517d0fd919b7626e581f9f5095c2
arinamiywee <arinazmeyka@gmail.com>
2025-10-26 14:55:55 +0300

    Ввод текста, проверка на соответствие критерия(минимум 100 символов)

--- ISIP523_Shirokova/Program.cs
+++ ISIP523_Shirokova/Program.cs
@@ -10,6 +10,18 @@ class TextAnalyzer
         Console.WriteLine("Анализ текста\n");
         while (true)
         {
+            Console.WriteLine("МЕНЮ:");
+            Console.WriteLine("1. Ввести текст");
+            Console.WriteLine("2. Количество слов");
+            Console.WriteLine("3. Самое короткое слово");
+            Console.WriteLine("4. Самое длинное слово");
+            Console.WriteLine("5. Количество предложений");
+            Console.WriteLine("6. Гласные и согласные");
+            Console.WriteLine("7. Частота букв");
+            Console.WriteLine("8. Полный анализ");
+            Console.WriteLine("9. Количество анализов");
+            Console.WriteLine("0. Выход");
+            Console.Write("Выберите: ");
             string choice = Console.ReadLine();
 
             switch (choice)
@@ -36,9 +48,23 @@ class TextAnalyzer
                     Console.WriteLine("Выход");
                     return;
                 default:
-                    Console.WriteLine("Неверный выбор!\n");
+                    Console.WriteLine("Неверный выбор!");
                     break;
             }
         }
+    }
+    static void InputText()
+    {
+        Console.Write("Введите текст (минимум 100 символов): ");
+        string text = Console.ReadLine();
+        
+        if (text.Length < 100)
+        {
+            Console.WriteLine("Ошибка, минимум 100 символов\n");
+            return;
+        }
+        
+        currentText = text;
+        Console.WriteLine("Текст сохранен!\n");
     }
 }
