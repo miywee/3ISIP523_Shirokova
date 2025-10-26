22905467ac360c13ea71358e76c990e0519ac957
arinamiywee <arinazmeyka@gmail.com>
2025-10-26 14:41:05 +0300

    Конструкция менюшки(switch case) для выбора действий под анализ текста

--- ISIP523_Shirokova/Program.cs
+++ ISIP523_Shirokova/Program.cs
@@ -1,2 +1,44 @@
-﻿
-Console.WriteLine("Hello, World"); 
+﻿using System;
+
+class TextAnalyzer
+{
+    static string currentText = "";
+    static int analysisCount = 0;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Анализ текста\n");
+        while (true)
+        {
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    break;
+                case "9":
+                    break;
+                case "0":
+                    Console.WriteLine("Выход");
+                    return;
+                default:
+                    Console.WriteLine("Неверный выбор!\n");
+                    break;
+            }
+        }
+    }
+}
