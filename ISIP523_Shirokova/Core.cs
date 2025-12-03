62865f6c0c49e198c0b91340947d5f28cebe0597
arinamiywee <arinazmeyka@gmail.com>
2025-12-03 16:27:58 +0300

    check2

---
+++ ISIP523_Shirokova/Core.cs
@@ -0,0 +1,6 @@
+namespace ISIP523_Shirokova;
+
+public class Core
+{
+    public static pr7.2Context Context = new pr7.2Context ();
+}
--- ISIP523_Shirokova/Program.cs
+++ ISIP523_Shirokova/Program.cs
@@ -1,2 +1,12 @@
 ﻿
+using System;
+using ISIP523_Shirokova;
+using ISIP523_Shirokova.Context;
+using ISIP523_Shirokova.Entities;
+
+
 Console.WriteLine("Hello, World!"); 
+List<Detail> details = Core.Context.Details.ToList();
+List<Garage> garages = Core.Context.Garages.ToList();
+List<GarageDetail> detailsgarages = Core.Context.DetailsGarages.ToList();
+
